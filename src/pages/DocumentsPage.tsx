18378c79ca107ea57ff06d0e1e39856faa7e8f29
furqanks <furqanks@gmail.com>
2025-06-18 16:46:18 +0500

    Update DocumentsPage.tsx logouticon delete

--- src/pages/DocumentsPage.tsx
+++ src/pages/DocumentsPage.tsx
@@ -27,11 +27,6 @@ const SettingsIcon = () => (
   </svg>
 );
 
-const LogoutIcon = () => (
-  <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-    <path fillRule="evenodd" d="M3 3a1 1 0 00-1 1v12a1 1 0 001 1h12a1 1 0 001-1V4a1 1 0 00-1-1H3zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z" clipRule="evenodd"></path>
-  </svg>
-);
 
 const SunIcon = () => (
   <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
